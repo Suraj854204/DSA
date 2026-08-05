0816738fe557e4b78617e54050dcd883b26c9213
suraj kumar singh <surajkumar854000@gmail.com>
2026-08-06 00:52:57 +0530

    Time: 9 ms (61.32%) | Memory: 47.3 MB (77.56%) - LeetSync

---
+++ 2881-split-strings-by-separator/split-strings-by-separator.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<String> splitWordsBySeparator(List<String> words, char separator) {
+       List<String> ans = new ArrayList<>();
+        for(String word:words){
+           String[]parts=word.split("\\"+separator);
+             
+            for(String part:parts){
+                if(!part.isEmpty()){
+                    ans.add(part);
+                }
+            }
+
+        }
+        return ans;
+    }
+}
