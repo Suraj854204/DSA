a44c6c07626692754eebda29a4543e707aee3a74
suraj kumar singh <surajkumar854000@gmail.com>
2026-09-12 14:16:21 +0530

    Time: 8 ms (24.71%) | Memory: 75.8 MB (85.98%) - LeetSync

---
+++ 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int waysToMakeFair(int[] nums) {
+        int n=nums.length;
+        int res=0;
+        int left[]=new int[2];
+        int right[]=new int[2];
+        for(int i=0;i<n;i++){
+            right[i%2]+=nums[i];
+        }
+        for(int i=0;i<n;i++){
+            right[i%2]-=nums[i];
+            if(left[0]+right[1]==left[1]+right[0])res++;
+             left[i%2]+=nums[i];
+
+        }
+        return res;
+
+    }
+}
