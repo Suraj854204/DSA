09867302c2eda874f45b1cba18ff0fb58e56789d
suraj kumar singh <surajkumar854000@gmail.com>
2026-09-21 03:50:52 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (49.77%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -13,10 +13,10 @@ class Solution {
         if(head==null||head.next==null){
             return head;
         }
-      ListNode newNode=reverseList(head.next);
-       head.next.next=head;
-       head.next=null;
-       return newNode;
+        ListNode temp=reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        return temp;
 
     }
 }
